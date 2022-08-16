3aef6127160647ce1748c64242f373a462f6b311
hep-mh <mail@hep-mh.com>
2022-08-16 14:14:43 +0200

    added shared preferences for later

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -14,6 +15,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   AudioplayersPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
