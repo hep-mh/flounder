68eb3cf757656c5084dd7d35f319210074e58d89
hep-mh <mail@hep-mh.com>
2022-08-18 11:10:39 +0200

    flounder -> Flounder

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# flounder
+# Flounder
 A cross-platform counter that can be used for the time-management in scientific talks
--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "flounder");
+    gtk_header_bar_set_title(header_bar, "Flounder");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "flounder");
+    gtk_window_set_title(window, "Flounder");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
