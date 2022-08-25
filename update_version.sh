3c0c54944af689c667e726b0855482605df3172e
hep-mh <mail@hep-mh.com>
2022-08-25 16:49:36 +0200

    added script to update the version

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 
-./replace.py raw/pubspec.yaml pubspec.yaml __VERSION__=$1
-./replace.py raw/com.hepmh.Flounder.metainfo.xml linux/com.hepmh.Flounder.metainfo.xml __VERSION__=$1 __DATE__=$(date +%Y-%m-%d)
-./replace.py raw/setup.iss windows/setup.iss __VERSION__=$1
+sed "s/__VERSION__/${1}/g" raw/pubspec.yaml > pubspec.yaml
+sed "s/__VERSION__/${1}/g;s/__DATE__/$(date +%Y-%m-%d)/g" raw/com.hepmh.Flounder.metainfo.xml > linux/com.hepmh.Flounder.metainfo.xml
+sed "s/__VERSION__/${1}/g" raw/setup.iss > windows/setup.iss
