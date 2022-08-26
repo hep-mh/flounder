838d92bcaf8df4108e75af0fd16b4336cb025119
hep-mh <mail@hep-mh.com>
2022-08-26 15:52:59 +0200

    small changes

---
+++ assets/generate_android_web.sh
@@ -0,0 +1,4 @@
+convert desktop-icon.png -resize 408x408 web-icon.png
+convert web-icon.png -bordercolor none -border 52 web-icon.png
+convert desktop-icon.png -resize 314x314 android-icon.png
+convert android-icon.png -bordercolor none -border 99 android-icon.png
