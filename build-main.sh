54bbd89bf034f58e0ab03af2736effdd23801567
hep-mh <mail@hep-mh.com>
2022-09-02 20:44:09 +0200

    appimage changes

--- build-main.sh
+++ build-main.sh
@@ -27,7 +27,7 @@ echo "Packaging application as .AppImage..."
 mkdir -p build/appimage
 cp -r build/linux/x64/release/bundle build/appimage/AppDir
 mkdir -p build/appimage/AppDir/usr/share/icons/hicolor/64x64/apps/
-convert assets/desktop-icon.png -resize 64x64 build/appimage/AppDir/usr/share/icons/hicolor/64x64/apps/appimage-icon.png
+convert assets/desktop-icon.png -resize 64x64 build/appimage/AppDir/usr/share/icons/hicolor/64x64/apps/com.hepmh.Flounder.png
 appimage-builder --recipe linux/AppImageBuilder.yml --appdir build/appimage/AppDir --build-dir build/appimage --skip-tests > build/appimage-builder.log 2>&1
 mv Flounder-latest-x86_64.AppImage packages/flounder-latest-linux-x86_64.AppImage
 
