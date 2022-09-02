c17c1b2442c221760b765837648942c24245c7a1
hep-mh <mail@hep-mh.com>
2022-09-02 23:01:19 +0200

    better structure for windows packaging

--- build-windows.ps1
+++ build-windows.ps1
@@ -9,5 +9,5 @@ flutter build windows
 Compress-Archive -Path "build\windows\Runner\Release\*" -DestinationPath "packages/flounder-latest-windows-x86_64.zip"
 
 # Creating .exe installer
-iscc .\windows\inno_setup.iss
+iscc .\windows\inno\inno_setup.iss
 Copy-Item "build\inno_setup\Flounder-Setup.exe" -Destination "packages/flounder-latest-windows-x86_64-setup.exe"
--- update_version.sh
+++ update_version.sh
@@ -3,4 +3,4 @@
 sed "s/__VERSION__/${1}/g" adaptable/README.md > README.md
 sed "s/__VERSION__/${1}/g" adaptable/pubspec.yaml > pubspec.yaml
 sed "s/__VERSION__/${1}/g;s/__DATE__/$(date +%Y-%m-%d)/g" adaptable/com.hepmh.Flounder.metainfo.xml > linux/flatpak/com.hepmh.Flounder.metainfo.xml
-sed "s/__VERSION__/${1}/g" adaptable/inno_setup.iss > windows/inno_setup.iss
+sed "s/__VERSION__/${1}/g" adaptable/inno_setup.iss > windows/inno/inno_setup.iss
