ffed37fed1b053c05007ef5d07263f03e68ab1a9
hep-mh <mail@hep-mh.com>
2022-09-03 14:00:30 +0200

    small formatting changes

--- update_version.sh
+++ update_version.sh
@@ -11,9 +11,15 @@ new_version=${1}
 new_date="$(date +%Y-%m-%d)"
 
 # Update the version for each file in the following array
-files=($(echo "README.md pubspec.yaml linux/flatpak/com.hepmh.Flounder.metainfo.xml windows/inno/inno_setup.iss VERSION"))
+files="
+    VERSION
+    README.md
+    pubspec.yaml
+    linux/flatpak/com.hepmh.Flounder.metainfo.xml
+    windows/inno/inno_setup.iss
+"
 
-for file in "${files[@]}"
+for file in $files
 do
-  sed -i "s/${old_version}/${new_version}/g;s/${old_date}/${new_date}/g" $file
+    sed -i "s/${old_version}/${new_version}/g;s/${old_date}/${new_date}/g" $file
 done
