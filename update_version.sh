f0eba4e6ade95c43be29c659abfe60c083c047c2
hep-mh <mail@hep-mh.com>
2022-09-03 13:58:22 +0200

    better way to change versions

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,19 @@
 #! /usr/bin/bash
 
-sed "s/__VERSION__/${1}/g" adaptable/README.md > README.md
-sed "s/__VERSION__/${1}/g" adaptable/pubspec.yaml > pubspec.yaml
-sed "s/__VERSION__/${1}/g;s/__DATE__/$(date +%Y-%m-%d)/g" adaptable/com.hepmh.Flounder.metainfo.xml > linux/flatpak/com.hepmh.Flounder.metainfo.xml
-sed "s/__VERSION__/${1}/g" adaptable/inno_setup.iss > windows/inno/inno_setup.iss
+file_content=($(cat VERSION))
+
+# Get the current version from file
+old_version=${file_content[0]}
+old_date=${file_content[1]}
+
+# Set the new version from command-line arguments
+new_version=${1}
+new_date="$(date +%Y-%m-%d)"
+
+# Update the version for each file in the following array
+files=($(echo "README.md pubspec.yaml linux/flatpak/com.hepmh.Flounder.metainfo.xml windows/inno/inno_setup.iss VERSION"))
+
+for file in "${files[@]}"
+do
+  sed -i "s/${old_version}/${new_version}/g;s/${old_date}/${new_date}/g" $file
+done
