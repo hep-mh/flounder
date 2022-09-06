36d758b69153d9dd84c6cd46add319671cacc1d2
hep-mh <mail@hep-mh.com>
2022-09-06 17:02:38 +0200

    closer to .deb support

--- build-main.sh
+++ build-main.sh
@@ -7,6 +7,11 @@ flutter pub run flutter_launcher_icons:main
 # flutter_launcher_icons does not currently support that
 convert assets/web-icon.png -resize 192x192 web/icons/Icon-maskable-192.png
 convert assets/web-icon.png -resize 512x512 web/icons/Icon-maskable-512.png
+# Resize the icons for use in the debian package
+for size in $(echo "16 32 64 128 256")
+do
+    convert assets/desktop-icon.png -resize ${size}x${size} linux/debian/flounder/usr/share/icons/hicolor/${size}x${size}/apps/flounder.png
+done
 
 # WEB
 echo -ne "\n\e[38;5;134m• Building web app •\e[0m\r"
