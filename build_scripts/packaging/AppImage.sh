ea08a9841e44bac62a6a87783bf4243ba3f900ca
hep-mh <mail@hep-mh.com>
2022-09-09 18:20:11 +0200

    faster appimage builds

--- build_scripts/packaging/AppImage.sh
+++ build_scripts/packaging/AppImage.sh
@@ -1,19 +1,21 @@
 #! /usr/bin/env bash
 
+APPDIR=build/appimage/AppDir
+
 # Clean the old build directory
 rm -rf build/appimage
 
 # Perform a fresh build
 mkdir -p build/appimage
-cp -r build/linux/x64/release/bundle build/appimage/AppDir
+cp -r build/linux/x64/release/bundle $APPDIR
 
-mkdir -p build/appimage/AppDir/usr/share/applications/
-cp linux/appimage/com.hepmh.Flounder.desktop build/appimage/AppDir
-cp linux/appimage/com.hepmh.Flounder.desktop build/appimage/AppDir/usr/share/applications/
+mkdir -p $APPDIR/usr/share/applications/
+cp linux/appimage/com.hepmh.Flounder.desktop $APPDIR
+cp linux/appimage/com.hepmh.Flounder.desktop $APPDIR/usr/share/applications/
 
-mkdir -p build/appimage/AppDir/usr/share/icons/hicolor/64x64/apps/
-convert assets/desktop-icon.png -resize 64x64 build/appimage/AppDir/com.hepmh.Flounder.png
-cp build/appimage/AppDir/com.hepmh.Flounder.png build/appimage/AppDir/usr/share/icons/hicolor/64x64/apps/
+mkdir -p $APPDIR/usr/share/icons/hicolor/64x64/apps/
+convert assets/desktop-icon.png -resize 64x64 $APPDIR/com.hepmh.Flounder.png
+cp $APPDIR/com.hepmh.Flounder.png $APPDIR/usr/share/icons/hicolor/64x64/apps/
 
-appimage-builder --recipe linux/appimage/AppImageBuilder.yml --appdir build/appimage/AppDir --build-dir build/appimage --skip-tests
+appimage-builder --recipe linux/appimage/AppImageBuilder.yml --appdir $APPDIR --build-dir build/appimage --skip-tests
 mv Flounder-latest-x86_64.AppImage packages/flounder-latest-linux-x86_64.AppImage
