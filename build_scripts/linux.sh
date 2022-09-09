4b55818f980fe5d26e123d64025cadc1c870cbc9
hep-mh <mail@hep-mh.com>
2022-09-09 15:14:27 +0200

    more changes to build scripts

--- build_scripts/linux.sh
+++ build_scripts/linux.sh
@@ -7,10 +7,10 @@ echo "Packaging application as .tar.gz..."
 tar czf packages/flounder-latest-debian-x86_64.tar.gz --directory=build/linux/x64/release/bundle/ .
 # Build a .flatpak file and save it in the packages/ directory
 echo "Packaging application as .flatpak..."
-./build_scripts/packaging/flatpak.sh
+./build_scripts/packaging/flatpak.sh > build/flatpak-builder.log 2>&1
 # Build an .AppImage file and save it in the packages/ directory
 #echo "Packaging application as .AppImage..."
-#./build_scripts/packaging/AppImage.sh
+#./build_scripts/packaging/AppImage.sh > build/appimage-builder.log 2>&1
 # Build an .deb file and save it in the packages/ directory
 #echo "Packaging application as .deb..."
 #./build_scripts/packaging/deb.sh
--- build_scripts/packaging/flatpak.sh
+++ build_scripts/packaging/flatpak.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 jsonnet linux/flatpak/com.hepmh.Flounder.jsonnet > linux/flatpak/com.hepmh.Flounder.json
-flatpak-builder --repo=build/flatpak_repo build/flatpak --force-clean linux/flatpak/com.hepmh.Flounder.json > build/flatpak-builder.log
+flatpak-builder --repo=build/flatpak_repo build/flatpak --force-clean linux/flatpak/com.hepmh.Flounder.json
 flatpak build-bundle build/flatpak_repo/ packages/flounder-latest-linux-x86_64.flatpak com.hepmh.Flounder
