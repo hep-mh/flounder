d5814ba8067a7caa9c55ed1dc707a3809ad1e3d1
Marco H <47532112+hep-mh@users.noreply.github.com>
2022-09-10 14:33:14 +0200

    Update windows.ps1

--- build_scripts/windows.ps1
+++ build_scripts/windows.ps1
@@ -1,7 +1,7 @@
 flutter build windows
 
-# Create a .zip file and save it in the packages/ directory
+# Create a .zip file and save it in the packages\ directory
 Compress-Archive -Path "build\windows\Runner\Release\*" -DestinationPath "packages/flounder-latest-windows-x86_64.zip" -Force
 
-# Create a .exe installer and save it in the packages/ directory
+# Create a .exe installer and save it in the packages\ directory
 .\build_scripts\packaging\exe.ps1 *> "build\exe.log"
