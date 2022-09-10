370953be73952911a63f6258b1184818017e8cfd
Marco H <47532112+hep-mh@users.noreply.github.com>
2022-09-10 14:31:33 +0200

    Update exe.ps1

--- build_scripts/packaging/exe.ps1
+++ build_scripts/packaging/exe.ps1
@@ -2,4 +2,4 @@
 iscc .\windows\inno\inno_setup.iss
 
 # Copy the installer to the packages\ directory
-Copy-Item "build\inno_setup\Flounder-Setup.exe" -Destination "packages/flounder-latest-windows-x86_64-setup.exe"
+Copy-Item "build\inno_setup\Flounder-Setup.exe" -Destination "packages\flounder-latest-windows-x86_64-setup.exe"
