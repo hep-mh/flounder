db846efeb515042362806dfa86995c0d9b40bfe4
hep-mh <mail@hep-mh.com>
2022-09-12 21:22:32 +0200

    msix support

--- build_scripts/pack/msix.ps1
+++ build_scripts/pack/msix.ps1
@@ -1,2 +1,7 @@
-# TODO
-# https://pub.dev/packages/msix
+# Get the publisher and the password for the certificate
+# pfx_info needs to be of the form
+# --publisher <...> --certificate-password <...>
+$args = Get-Content .\windows\msix\pfx_info -Raw
+
+# Create the package
+flutter pub run msix:create $args
--- build_scripts/windows.ps1
+++ build_scripts/windows.ps1
@@ -5,5 +5,9 @@ Write-Output "Packaging application as .zip..."
 Compress-Archive -Path "build\windows\Runner\Release\*" -DestinationPath "packages/flounder-latest-windows-x86_64.zip" -Force
 
 # Create a .exe installer and save it in the packages\ directory
-Write-Output "Packaging application as .exe (installer)..."
+Write-Output "Creating .exe installer..."
 .\build_scripts\pack\exe.ps1 *> "build\exe.log"
+
+# Create an .msix installer and save it in the packages\ directory
+Write-Output "Creating .msix installer..."
+.\build_scripts\pack\msix.ps1 *> "build\msix.log"
