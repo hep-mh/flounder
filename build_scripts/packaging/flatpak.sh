e53c6f50676b99b91123b17d20985b91447a5698
hep-mh <mail@hep-mh.com>
2022-09-12 09:19:24 +0200

    sign flatpak repo

--- build_scripts/packaging/flatpak.sh
+++ build_scripts/packaging/flatpak.sh
@@ -6,7 +6,7 @@
 jsonnet linux/flatpak/com.hepmh.Flounder.jsonnet > linux/flatpak/com.hepmh.Flounder.json
 
 # Create the flatpak repository
-flatpak-builder --repo=build/flatmh/ build/flatpak/ --force-clean linux/flatpak/com.hepmh.Flounder.json
+flatpak-builder --gpg-sign=87505F6E6AA515DC --repo=build/flapmh/ build/flatpak/ --force-clean linux/flatpak/com.hepmh.Flounder.json
 
 # Build the bundle from the repository in the packages/ directory
-flatpak build-bundle build/flatmh/ packages/flounder-latest-linux-x86_64.flatpak com.hepmh.Flounder
+flatpak build-bundle build/flapmh/ packages/flounder-latest-linux-x86_64.flatpak com.hepmh.Flounder
