139f471bf7a5327db2a335635299f51c14b795f2
hep-mh <mail@hep-mh.com>
2022-09-12 09:35:52 +0200

    echo to windows build

--- build_scripts/windows.ps1
+++ build_scripts/windows.ps1
@@ -1,7 +1,9 @@
 flutter build windows
 
 # Create a .zip file and save it in the packages\ directory
+Write-Output "Packaging application as .zip..."
 Compress-Archive -Path "build\windows\Runner\Release\*" -DestinationPath "packages/flounder-latest-windows-x86_64.zip" -Force
 
 # Create a .exe installer and save it in the packages\ directory
+Write-Output "Packaging application as .exe (installer)..."
 .\build_scripts\packaging\exe.ps1 *> "build\exe.log"
